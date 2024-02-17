a198782cd407e8dea0045849d49610d9b2a45abe
Darius Bruno <dbruno5@myseneca.ca>
2024-02-16 23:53:56 -0500

    assignment 2

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -1,28 +1,71 @@
+/********************************************************************************
+* WEB322 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Darius Bruno Student ID: 047701073 Date: 2/16/2024
+*
+* Published URL: https://prussian-blue-sea-lion-hose.cyclic.app/
+*
+********************************************************************************/
+
 const setData = require("../data/setData");
 const themeData = require("../data/themeData");
 
 let sets = [];
 
+
 const initialize = () => {
-    setData.forEach(setDataObj => {
-        const themeObj = themeData.find(theme => theme.id === setDataObj.theme_id);
-        if (themeObj) {
-            sets.push({ ...setDataObj, theme: themeObj.name });
+    return new Promise((resolve, reject) => {
+        try {
+            setData.forEach(setDataObj => {
+                const themeObj = themeData.find(theme => theme.id === setDataObj.theme_id);
+                if (themeObj) {
+                    sets.push({ ...setDataObj, theme: themeObj.name });
+                }
+            });
+            resolve();
+        } catch (error) {
+            reject("Initialization failed: " + error.message);
         }
     });
 };
 
-const getAllSets = () => sets;
+const getAllSets = () => {
+    return new Promise((resolve, reject) => {
+        if (sets.length > 0) {
+            resolve(sets);
+        } else {
+            reject("Error: Sets array problem");
+        }
+    });
+};
 
 const getSetByNum = setNum => {
-    const foundSet = sets.find(set => set.set_num === setNum);
-    return foundSet ? foundSet : "Error! No sets have been found with that number.";
+    return new Promise((resolve, reject) => {
+        const foundSet = sets.find(set => set.set_num === setNum);
+        if (foundSet) {
+            resolve(foundSet);
+        } else {
+            reject("Error! No sets have been found with that number.");
+        }
+    });
 };
 
 const getSetsByTheme = theme => {
-    const foundSets = sets.filter(set => set.theme.toLowerCase().includes(theme.toLowerCase()));
-    return foundSets.length > 0 ? foundSets : "Error! No sets have been found with that theme.";
+    return new Promise((resolve, reject) => {
+        const foundSets = sets.filter(set => set.theme.toLowerCase().includes(theme.toLowerCase()));
+        if (foundSets.length > 0) {
+            resolve(foundSets);
+        } else {
+            reject("Error! No sets have been found with that theme.");
+        }
+    });
 };
+
 
 //initialize();
 //console.log(getAllSets());
--- server.js
+++ server.js
@@ -1,28 +1,121 @@
+/********************************************************************************
+* WEB322 – Assignment 02
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
+*
+* Name: Darius Bruno Student ID: 047701073 Date: 2/16/2024
+*
+* Published URL: https://prussian-blue-sea-lion-hose.cyclic.app/
+*
+********************************************************************************/
+
 const express = require('express');
+const path = require('path');
+
 const legoData = require("./modules/legoSets");
 const HTTP_PORT = process.env.PORT || 8080;
 const app = express();
 
-legoData.initialize();
-
-app.get('/', (req, res) => {
-    res.send("Assignment 2: Darius Bruno - 047701073");
-})
-
-app.get('/lego/sets', (req, res) => {
-    res.json(legoData.getAllSets());
-});
-
-app.get('/lego/sets/num-demo', (req, res) => {
-    const set = legoData.getSetByNum("03-1"); 
-    res.send(set); 
-});
+app.use(express.static('public'));
 
 
-app.get('/lego/sets/theme-demo', (req, res) => {
-    const sets = legoData.getSetsByTheme("tech"); 
-    res.send(sets); 
-});
+
+async function startServer() {
+    try {
+        await legoData.initialize();
+        console.log('Initialization successful');
+
+                
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, 'views', 'home.html'));
+        })
+
+        app.get('/about', (req, res) => {
+            res.sendFile(path.join(__dirname, 'views', 'about.html'));
+        })
+
+        app.get('/404', (req, res) => {
+            res.sendFile(path.join(__dirname, 'views', '404.html'));
+        })
+
+        app.get('/lego/sets', (req, res) => {
+            legoData.getAllSets()
+                .then(sets => res.json(sets))
+                .catch(err => res.status(500).send(err));
+        });
+
+        app.get('/lego/sets/02-2', (req, res) => {
+            legoData.getSetByNum("02-2")
+                .then(set => res.json(set))
+                .catch(err => res.status(404).send(err));
+        });
+
+        app.get('/lego/sets/022-1', (req, res) => {
+            legoData.getSetByNum("022-1")
+                .then(set => res.json(set))
+                .catch(err => res.status(404).send(err));
+        });
+
+        app.get('/lego/sets/021-1', (req, res) => {
+            legoData.getSetByNum("021-1")
+                .then(set => res.json(set))
+                .catch(err => res.status(404).send(err));
+        });
 
 
-app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+        app.get('/lego/sets/:setNum', (req, res) => {
+            const setNum = req.params.setNum;
+            legoData.getSetByNum(setNum)
+                .then(set => res.json(set))
+                .catch(err => res.status(404).send(`Error: ${err}`));
+        });
+
+
+
+        app.get('/lego/sets/theme/:themeName', (req, res) => {
+            const themeName = req.params.themeName;
+            legoData.getSetsByTheme(themeName)
+                .then(sets => res.json(sets))
+                .catch(err => res.status(404).send(err));
+        });
+
+        app.get('/lego/sets/:setNum', (req, res) => {
+            legoData.getSetByNum(req.params.setNum)
+                .then(set => res.json(set))
+                .catch(err => res.status(404).send(`Error: ${err}`));
+        });
+
+
+        app.get('/lego/sets/num-demo', (req, res) => {
+            legoData.getSetByNum("03-1")
+                .then(set => res.json(set))
+                .catch(err => res.status(404).send(err));
+        });
+
+        app.use((req, res) => {
+            res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+        });
+
+        app.get('/lego/sets', (req, res) => {
+            if (req.query.theme) {
+                legoData.getSetsByTheme(req.query.theme)
+                    .then(sets => res.json(sets))
+                    .catch(err => res.status(404).send(err));
+            } else {
+                legoData.getAllSets()
+                    .then(sets => res.json(sets))
+                    .catch(err => res.status(500).send(err));
+            }
+        });
+
+        app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+            } catch (err) {
+                console.error('Initialization failed:', err);
+            }
+        }
+
+startServer();
+
